608cf892ebe8e003e316ef982d941d19dd5c2903
Fran <fmarchantemera@safareyes.es>
2026-02-09 20:28:42 +0100

    parece que ya

--- src/main/java/com/aplicacion/eventos_escolares/servicios/NotificacionesService.java
+++ src/main/java/com/aplicacion/eventos_escolares/servicios/NotificacionesService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @Service
 public class NotificacionesService {
+    //Esto es solo un comentario para comprobar github
     @Autowired
     private NotificacionesRepository notificacionesRepository;
 
